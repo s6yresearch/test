b188605eae96d8ad967134b97f12c256e30cbaaf
s6yresearch <sh@s6y-research.com>
2012-06-19 19:28:37 +0200

    test3

--- src/com/s6yresearch/test/MainClass.java
+++ src/com/s6yresearch/test/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Welt");
+		System.out.println("Hallo Wel");
 	}
 
 }
