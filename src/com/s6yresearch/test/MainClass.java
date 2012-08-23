e9798deab468a8e87136fa65ac6c7292e713fad9
Hentschel <sebastian.hentschel@gmx.de>
2012-08-23 19:44:54 +0200

    test

--- src/com/s6yresearch/test/MainClass.java
+++ src/com/s6yresearch/test/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Weltrr");
+		System.out.println("Hallo Welt");
 	}
 
 }
